2579fa058653db0d9a0b7c15adccfeacdbc6ff17
Fatemeh Eshghi <157709672+Fatemeh-Eshghi@users.noreply.github.com>
2025-07-22 00:28:16 +0300

    vite-typescript

---
+++ src/components/MyList.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+type TItem = {
+  id: string;
+  text: string;
+  clicked?: boolean;
+};
+
+interface ListProps {
+  header: string;
+  items: TItem[];
+  updateList: (id: string) => void;
+}
+
+const MyList: React.FC<ListProps> = ({ header, items, updateList }) => {
+  return (
+    <div>
+      <h2>{header}</h2>
+      <ol>
+        {items.map((item) => (
+          <li
+            key={item.id}
+            style={{ textDecoration: item.clicked ? "line-through" : "none", cursor: "pointer" }}
+            onClick={() => updateList(item.id)}
+          >
+            {item.text}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default MyList;
